5cac6e4a77bac80f2c0996a610ef40956dfaa016
zee <xeshan.ahmed@gmail.com>
2019-08-08 21:34:35 +0500

    email

---
+++ email/opendkim.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+if [ ! -d /etc/opendkim/keys ]; then
+    sleep 9999999999999999999
+elif [ -z "$(find /etc/opendkim/keys -type f ! -name .)" ]; then
+    sleep 9999999999999999999
+else
+    /usr/sbin/opendkim -D -f -x /etc/opendkim/opendkim.conf
+fi
+
+
