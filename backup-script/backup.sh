213d0922f39a931ee420af613711846857e7dbe9
zee <xeshan.ahmed@gmail.com>
2019-09-07 19:28:52 +0500

    script changes

--- backup-script/backup.sh
+++ backup-script/backup.sh
@@ -9,12 +9,18 @@ DATE=$(date +"%Y-%m-%d")
 source "/etc/keys"
 export AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY
+export MYSQL_HOST
+export MYSQL_USER
+export MYSQL_PASSWORD
+export DATABASE_NAME
+export MYSQL_PORT
+export DOCKER_COMPOSE
 
 mkdir -p ${BACKUP_DIR}/${DATE}
 
 backup_mysql(){
-		cd ${DC_DIR}
-		${DOCKER_COMPOSE} exec db \
+        cd ${DC_DIR}
+        ${DOCKER_COMPOSE} exec -T db \
         mysqldump -h ${MYSQL_HOST} \
            -P ${MYSQL_PORT} \
            -u ${MYSQL_USER} \
@@ -26,7 +32,6 @@ backup_files(){
 }
 
 upload_s3(){
-	echo ${BACKUP_DIR}/${DATE}
         aws s3 cp ${BACKUP_DIR}/${DATE} ${AMAZON_S3_BUCKET}${DATE} --recursive
 }
 
